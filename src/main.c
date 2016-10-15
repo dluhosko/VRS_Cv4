58caba9c4d09e224589878fe610ba849fee3f6b8
Viktor Dluhoš <dluhosko@gmail.com>
2016-10-15 20:06:26 +0200

    Add commitment
    VRS Cv4 - Inicializacia AD prevodnika (upravena funkcia adc_init() )

--- src/main.c
+++ src/main.c
@@ -58,12 +58,56 @@ void LED_init (void)
 	  GPIO_Init(GPIOA,&GPIOInitStruct);
 }
 
+void adc_init(void)
+{
+GPIO_InitTypeDef GPIO_InitStructure;
+ADC_InitTypeDef ADC_InitStructure;
+/* Enable GPIO clock */
+RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);//Opraviù a upraviù
+/* Configure ADCx Channel 2 as analog input */
+//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 ;
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 ;
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+GPIO_Init(GPIOA, &GPIO_InitStructure);
+/* Enable the HSI oscillator */
+RCC_HSICmd(ENABLE);
+/* Check that HSI oscillator is ready */
+while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
+/* Enable ADC clock */
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+/* Initialize ADC structure */
+ADC_StructInit(&ADC_InitStructure);
+/* ADC1 configuration */
+ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+ADC_InitStructure.ADC_NbrOfConversion = 1;
+ADC_Init(ADC1, &ADC_InitStructure);
+/* ADCx regular channel8 configuration */
+ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_16Cycles);
+/* Enable the ADC */
+ADC_Cmd(ADC1, ENABLE);
+/* Wait until the ADC1 is ready */
+while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET)
+{
+}
+/* Start ADC Software Conversion */
+//ADC_SoftwareStartConv(ADC1);
+}
+
 int main(void)
 {
+	  int i = 0;
+	  int AD_value=0;
+	  int help=0;
 
 
 	//Inicializacia LED
 	LED_init();
+	//Inicializacia AD prevodnika
+    adc_init();
 
   /*
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
